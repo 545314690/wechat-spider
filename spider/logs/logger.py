6fafad33ff874ac099648aa68eee2508a36db9c8
545314690 <545314690@qq.com>
2017-07-25 14:39:06 +0800

    fix logs bug

--- spider/logs/logger.py
+++ spider/logs/logger.py
@@ -2,7 +2,7 @@
 import yaml
 import logging.config
 import os
-
+config_path = os.path.join(os.path.dirname(__file__), 'logging.yaml')
 def setup_logging(default_path = "logging.yaml",default_level = logging.INFO,env_key = "LOG_CFG"):
     path = default_path
     value = os.getenv(env_key,None)
@@ -14,6 +14,6 @@ def setup_logging(default_path = "logging.yaml",default_level = logging.INFO,env
     else:
         logging.basicConfig(level = default_level)
 
-setup_logging(default_path="../logging.yaml",default_level=logging.INFO)
+setup_logging(default_path=config_path,default_level=logging.INFO)
 
 logger = logging.getLogger(__name__)
